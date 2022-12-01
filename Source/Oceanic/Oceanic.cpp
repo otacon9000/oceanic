ed0b4a9c5d2ea74f1dda58ccd5022a45017b689c
Andrea Alicino <andrea.alicino@gmail.com>
2022-11-30 21:50:27 -0500

    upload blank project

---
+++ Source/Oceanic/Oceanic.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Oceanic.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Oceanic, "Oceanic" );
